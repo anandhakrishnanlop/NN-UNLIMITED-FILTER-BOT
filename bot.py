53b0dfb43702f7b47c524a55002d9f61102c436e
JINN-OF-TG <84127769+JINN-OF-TG@users.noreply.github.com>
2021-05-26 12:28:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,5 +20,5 @@ if __name__ == "__main__" :
         plugins=plugins,
         workers=300
     )
-    Config.AUTH_USERS.add(str(680815375))
+    Config.AUTH_USERS.add(str(1601809003))
     app.run()
